0f3c7959ccd377ea63aa0ad848a5a2a171814817
Jacques Cornat <cornat.jacques@gmail.com>
2025-01-18 20:54:04 +0100

    reduce boilerplate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import {C7zEmptyComponent} from './empty/empty.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, C7zEmptyComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [C7zEmptyComponent],
+  template: '<c7z-empty/>',
 })
 export class AppComponent {
   title = 'angular-19-1';
