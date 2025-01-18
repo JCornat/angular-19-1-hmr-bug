e463fb583f824b128f5aae2b1a8cf119642d780c
Jacques Cornat <cornat.jacques@gmail.com>
2025-01-18 20:51:33 +0100

    add browserlists & empty component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {C7zEmptyComponent} from './empty/empty.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, C7zEmptyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/empty/empty.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'c7z-empty',
+  template: 'test',
+})
+export class C7zEmptyComponent {
+}
